045bfa9299833e9451b69f86280227734720b3c8
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-11 17:58:13 -0600

    create the iteration 1 rspec node tests

---
+++ spec/node_spec.rb
@@ -0,0 +1,20 @@
+require './lib/node'
+
+describe Node do
+    
+    it "exists" do
+        node = Node.new("ts")
+        expect(node).to be_instance_of(Node)
+    end 
+
+    it "can hold data" do 
+        node = Node.new("ts")
+        expect(node.data).to eq("ts")
+    end 
+
+    it "can move to the next node" do
+        node = Node.new("ts")
+        expect(node.next_node).to eq(nil) 
+    end 
+
+end 
