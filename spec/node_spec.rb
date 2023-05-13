23d394b6ac3ca9da71ab97653a8f4d69221256fb
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-13 13:25:31 -0600

    fix spacing of node spec to fit ruby conventions

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,18 +2,18 @@ require 'rspec'
 require './lib/node'
 
 describe Node do
-    it "exists" do
-        node = Node.new("ts")
-        expect(node).to be_instance_of(Node)
-    end 
+  it "exists" do
+    node = Node.new("ts")
+    expect(node).to be_instance_of(Node)
+  end 
 
-    it "can hold data" do 
-        node = Node.new("ts")
-        expect(node.data).to eq("ts")
-    end 
+  it "can hold data" do 
+    node = Node.new("ts")
+    expect(node.data).to eq("ts")
+  end 
 
-    it "next_node_pointer default value is nil" do
-        node = Node.new("ts")
-        expect(node.next_node_pointer).to eq(nil) 
-    end 
+  it "next_node_pointer default value is nil" do
+    node = Node.new("ts")
+    expect(node.next_node_pointer).to eq(nil) 
+  end 
 end 
