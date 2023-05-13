4212af7982e49781290558075fbbfcd481704c59
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-13 13:32:55 -0600

    create the count method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,29 +1,38 @@
 class LinkedList
-
-    attr_reader :head
+  attr_reader :head
   
-    def initialize
-      @head = nil
-    end
-  
-    def append(data)
-      new_node = Node.new(data)
-  
-      if @head == nil 
-        @head = new_node
-      else
-        current_node = @head
-            until current_node.next_node_pointer == nil 
-            current_node = current_node.next_node_pointer
-            end
-
-        current_node.next_node_pointer = new_node
-      end
-    end
+  def initialize
+    @head = nil
   end
   
+  def append(data)
+    new_node = Node.new(data)
+  
+    if @head == nil 
+       @head = new_node
+    else
+      current_node = @head
+      until current_node.next_node_pointer == nil 
+        current_node = current_node.next_node_pointer
+      end
 
-        # count idea : 
-        # loop current node like in append. 
-        # nodes 
-        # nodes +1 and return that number  
+      current_node.next_node_pointer = new_node
+    end
+  end
+
+  def count
+    if @head == nil 
+      return 0
+
+    else 
+      counter = 1
+      current_node = @head 
+      until current_node.next_node_pointer == nil 
+        current_node = current_node.next_node_pointer
+        counter += 1
+      end
+      return counter
+    end 
+  end 
+end
+  
