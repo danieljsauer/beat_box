04b16fa26633533f22ac6633aeb877c8277a2367
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-13 13:20:31 -0600

    fix node class spacing to fit ruby conventions

--- lib/node.rb
+++ lib/node.rb
@@ -1,14 +1,9 @@
-class Node 
+class Node
+  attr_reader :data
+  attr_accessor :next_node_pointer
 
-    attr_reader :data
-    attr_accessor :next_node_pointer 
-    #I am using accessor for next_node because the linked_list will be altering this data, but I need it to be stored within the node. Unsure if this is the correct appraoch.
-
-    def initialize(data)
-        @data = data
-        @next_node_pointer = nil  
-        #next_node is nil since nil is what indicates the tail of the linked list. Unless we specify, we will be adding nodes to the back of the train.
-        #next node is how we will move around the list. This attribute is VERY confusing to me. 
-    end 
-
-end 
+  def initialize(data)
+    @data = data
+    @next_node_pointer = nil
+  end
+end
