62485238daac91b439d010a4bfb07d9472cdcc2c
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-14 22:14:38 -0600

    create beatbox prepend method tests

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -19,7 +19,7 @@ describe BeatBox do
   describe "LinkedList methods" do 
 
     describe "count" do 
-      it "counts the number of node" do 
+      it "counts the number of nodes" do 
         bbox = BeatBox.new
         bbox.list.append("bts")
         bbox.list.append("pts")
@@ -35,16 +35,36 @@ describe BeatBox do
         expect(bbox.list.to_string).to eq ("bts pts kuh ts")
         expect(bbox.count).to eq(4)
       end 
+      it "skips words that contain too many characters" do
+        bbox = BeatBox.new
+        bbox.append("bts kuh 0123456789012 tsh")
+        expect(bbox.list.to_string).to eq("bts kuh tsh")
+        expect(bbox.count).to eq(3)
+      end 
+      it "only accepts string data" do 
+        bbox = BeatBox.new
+        expect(bbox.append(42)).to eq("Invalid Entry")
+      end
     end
 
     describe "prepend" do 
-      xit "adds the first word as the first node" do 
+      it "adds the first word as the first node" do 
         bbox = BeatBox.new
         bbox.append("ts")
         bbox.prepend("bts pts kuh")
         expect(bbox.list.to_string).to eq ("bts pts kuh ts")
         expect(bbox.count).to eq(4)
       end 
+      it "skips words that contain too many characters" do
+        bbox = BeatBox.new
+        bbox.prepend("bts kuh 0123456789012 tsh")
+        expect(bbox.list.to_string).to eq("bts kuh tsh")
+        expect(bbox.count).to eq(3)
+      end 
+      it "only accepts string data" do 
+        bbox = BeatBox.new
+        expect(bbox.prepend(42)).to eq("Invalid Entry")
+      end
     end 
 
   end 
