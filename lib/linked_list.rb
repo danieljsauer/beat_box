6af072a21eb0e761012fad41968ecd28cf7ffd6d
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-14 19:31:27 -0600

    edit line breaks and spacing to make code more readable

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,6 +7,7 @@ class LinkedList
   
   def append(data)
     if data.is_a?(String) && data.length <= 10 
+
       new_node = Node.new(data)
   
       if @head == nil 
@@ -16,9 +17,9 @@ class LinkedList
         until current_node.next_node_pointer == nil 
           current_node = current_node.next_node_pointer
         end
-
         current_node.next_node_pointer = new_node
       end
+
     else 
       return "Invalid Entry"
     end 
@@ -26,9 +27,11 @@ class LinkedList
 
   def prepend(data) 
     if data.is_a?(String) && data.length <= 10 
+
       old_head = @head 
       @head = Node.new(data)
       head.next_node_pointer = old_head
+
     else 
       return "Invalid Entry"
     end 
@@ -36,8 +39,10 @@ class LinkedList
 
   def insert(index, data)
     if data.is_a?(String) && data.length <= 10 && index.is_a?(Integer) && index <= count 
+
       if index == 0
         prepend(data)
+
       else
         new_node = Node.new(data)
         prior_node = @head 
@@ -47,6 +52,7 @@ class LinkedList
         new_node.next_node_pointer = prior_node.next_node_pointer
         prior_node.next_node_pointer = new_node
       end    
+      
     else 
       return "Invalid Entry"
     end 
@@ -70,6 +76,7 @@ class LinkedList
   def to_string 
     if @head == nil 
       return "No items in list"
+
     else
       items = []
       items << head.data 
@@ -79,14 +86,15 @@ class LinkedList
         items << current_node.data 
       end
     end
+
     items = items.join(" ")
     return items
   end  
 
   def find(index, number)
     @head == nil ? (return "List is empty") : nil 
-
     if index.is_a?(Integer) && index <= count && number.is_a?(Integer)
+
       selected = []
       current_node = @head 
       index.times do 
@@ -96,11 +104,13 @@ class LinkedList
         if current_node == nil 
           break
         else 
-        selected << current_node.data
-        current_node = current_node.next_node_pointer
-        end
+          selected << current_node.data
+          current_node = current_node.next_node_pointer
+        end  
       end  
+
       selected = selected.join(" ")
+
     else 
       return "Invalid Entry"
     end 
@@ -108,17 +118,19 @@ class LinkedList
 
   def includes?(data)
     @head == nil ? (return "List is empty") : nil 
-
     if data.is_a?(String) && data.length <= 10
+
       current_node = @head 
       until current_node.next_node_pointer == nil 
         if data == current_node.data 
           return true 
         else 
-        current_node = current_node.next_node_pointer 
+          current_node = current_node.next_node_pointer 
         end 
       end 
+
       return false 
+      
     else 
       return "Invalid Entry"
     end 
@@ -126,15 +138,17 @@ class LinkedList
 
   def pop 
     @head == nil ? (return "List is empty") : nil 
+
     if count <= 1  
       @head = nil
-    else  prior_node = @head 
+
+    else prior_node = @head 
     (count - 2).times do 
-      prior_node = prior_node.next_node_pointer
+        prior_node = prior_node.next_node_pointer
       end  
       prior_node.next_node_pointer = nil
     end 
-    end 
-
+  end 
+  
 end
   
