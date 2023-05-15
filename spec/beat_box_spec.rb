084cd13300b5604dfec44fb3b2e68fe6687f63e7
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-14 20:27:30 -0600

    fix the syntax error

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -4,17 +4,15 @@ require './lib/beat_box'
 require './lib/node'
 require './lib/linked_list'
 
-describe BeatBox
+describe BeatBox do 
   describe "initialize" do
-    xit "exists" do
+    it "exists" do
       bbox = BeatBox.new
       expect(bbox).to be_instance_of(BeatBox)  
     end 
-    xit "creates a list object" do
+    it "creates a list object" do
       bbox = BeatBox.new
       expect(bbox.list).to be_instance_of(LinkedList)
     end 
-
   end 
-
 end 
